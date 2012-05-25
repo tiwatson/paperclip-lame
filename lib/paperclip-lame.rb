a5323541cd26943e1a4d61e5158b13c2d9fe8a31
Tim Watson <tiwatson@gmail.com>
2012-05-25 13:23:45 -0400

    Initial commit

---
+++ lib/paperclip-lame.rb
@@ -0,0 +1,61 @@
+module Paperclip
+  class Ffmpeg < Processor
+    attr_accessor :geometry, :format, :whiny, :convert_options
+
+    # Creates a Video object set to work on the +file+ given. It
+    # will attempt to transcode the video into one defined by +target_geometry+
+    # which is a "WxH"-style string. +format+ should be specified.
+    # Video transcoding will raise no errors unless
+    # +whiny+ is true (which it is, by default. If +convert_options+ is
+    # set, the options will be appended to the convert command upon video transcoding.
+    def initialize file, options = {}, attachment = nil
+      @convert_options = {
+        :input => {},
+        :output => { :y => nil }
+      }
+      unless options[:convert_options].nil? || options[:convert_options].class != Hash
+        unless options[:convert_options][:input].nil? || options[:convert_options][:input].class != Hash
+          @convert_options[:input].reverse_merge! options[:convert_options][:input]
+        end
+        unless options[:convert_options][:output].nil? || options[:convert_options][:output].class != Hash
+          @convert_options[:output].reverse_merge! options[:convert_options][:output]
+        end
+      end
+      
+      @file            = file
+
+      @whiny           = options[:whiny].nil? ? true : options[:whiny]
+      @format          = options[:format]
+      @time            = options[:time].nil? ? 3 : options[:time]
+      @current_format  = File.extname(@file.path)
+      @basename        = File.basename(@file.path, @current_format)
+    end
+    # Performs the transcoding of the +file+ into a thumbnail/video. Returns the Tempfile
+    # that contains the new image/video.
+    def make
+      src = @file
+      dst = Tempfile.new([@basename, @format ? ".#{@format}" : ''])
+      dst.binmode
+      
+
+      parameters = []
+      
+      # Add source
+      parameters << @convert_options[:input].map { |k,v| "-#{k.to_s} #{v} "}
+      parameters << "-f --quiet :source"
+      parameters << @convert_options[:output].map { |k,v| "-#{k.to_s} #{v} "}
+      parameters << ":dest"
+
+      parameters = parameters.flatten.compact.join(" ").strip.squeeze(" ")
+      
+      Paperclip.log("[lame] #{parameters}")
+      begin
+        success = Paperclip.run("lame", parameters, :source => "#{File.expand_path(src.path)}", :dest => File.expand_path(dst.path))
+      rescue Cocaine::ExitStatusError => e
+        raise PaperclipError, "error while processing video for #{@basename}: #{e}" if @whiny
+      end
+
+      dst
+    end
+  end
+end
---
+++ paperclip-lame.gemspec
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+$:.push File.expand_path("../lib", __FILE__)
+
+Gem::Specification.new do |s|
+  s.name        = "paperclip-lame"
+  s.version     = '0.1.0'
+  s.platform    = Gem::Platform::RUBY
+  s.authors     = ["Tim Watson"]
+  s.email       = ["tiwatson@gmail.com"]
+  s.homepage    = "http://github.com/tiwatson/paperclip-lame"
+  s.summary     = %q{Process your attachments with lame}
+  s.description = %q{Process your attachments with lame}
+
+  #s.rubyforge_project = "paperclip-lame"
+
+  s.files         = `git ls-files`.split("\n")
+  s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
+  s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  s.require_paths = ["lib"]
+  
+  s.add_dependency('paperclip', '>=2.5.2')
+end
